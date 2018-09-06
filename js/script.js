06c7f3cfa7ce89283358d1f5395f74415af6d49d
maria trepakova <932400610@cit3-501m-08.acg.ac.nz>
2018-09-06 15:20:34 +1200

    section 4: map, section 3: icons

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // Map
 
+$(()=>{
+ 	
+ 	var center = {lat: -45.031449, lng: 168.661904};
+ 	var map = L.map('map').setView(center, 17);
+
+ 	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(map);
+});
