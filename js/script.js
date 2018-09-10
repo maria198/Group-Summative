a3ceb61c52af479d443e94e33bb38cef8e542173
claire elizabeth stark <932400160@cit3-501m-07.acg.ac.nz>
2018-09-10 13:02:52 +1200

    added in new section 1 for Ryan

--- js/script.js
+++ js/script.js
@@ -7,3 +7,5 @@ $(()=>{
 
  	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(map);
 });
+
+
