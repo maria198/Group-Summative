45c70eb9abfc4316205d1ceff8355094ad3cd706
claire elizabeth stark <932400160@cit3-501m-07.acg.ac.nz>
2018-09-12 14:57:32 +1200

    locked map in postition

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 
 $(()=>{
  	
- 	var center = {lat: -45.031449, lng: 168.661904};
- 	var map = L.map('map').setView(center, 17);
+ 	let center = [-36.8446152873055,174.76662397384644];
+ 	let map = L.map('map',{zoomControl:true,scrollWheelZoom:false}).setView(center, 17);
 
  	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(map);
 });
