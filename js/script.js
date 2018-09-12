63ceb81c5a34c2bbe22c0f04b1cda95324f49b40
Catriona ting yan Chan <845870007@cit3-501m-11.acg.ac.nz>
2018-09-12 16:44:24 +1200

    map relocates with dropdown, page names are changed on click with dropdown

--- js/script.js
+++ js/script.js
@@ -10,14 +10,13 @@ const key = version + clientid + clientSecret;
 
 $(()=>{
 
-	var potato = {lat: -36.86667, lng: 174.76667}
+	var defaultLocation = {lat: -36.86667, lng: 174.76667}
 
-	var map = L.map('map').setView(potato, 13);
+	var map = L.map('map').setView(defaultLocation, 13);
 	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(map);
 
- 	var center = $('.dropdown-item').on('click', function(){
+ 	var locateMe = $('.dropdown-item').on('click', function(){
 
-		var location = [];
  		var lat = $(this).data('lat');
  		var lng = $(this).data('lng');
 
@@ -25,9 +24,20 @@ $(()=>{
 
 		map.setView(new L.LatLng(lat, lng), 13);
 
+		var location = $(this)[0].innerText;
+		console.log(location);
 
+	    $('.location-title h3, .dropdown-toggle').text(location);
 	});
 
+ 	var chooseChoice = $('.choices').on('click',function(){
+ 		var choice = $(this).data('choice');
+
+ 		var choiceName = $(this)[0].innerText;
+
+ 		$('.location-choice h3').text(choiceName);
+
+ 	});
 
 });
 
