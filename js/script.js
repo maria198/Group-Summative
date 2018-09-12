a4a0bc623bc42e9d885a07ac83abb38e3a79ec31
maria trepakova <932400610@cit3-501m-08.acg.ac.nz>
2018-09-12 12:37:58 +1200

    started js, finished dropdown list for suburbs

--- js/script.js
+++ js/script.js
@@ -6,6 +6,38 @@ $(()=>{
  	var map = L.map('map').setView(center, 17);
 
  	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(map);
+
+ 	$('.dropdown-item').each(function(){
+ 		$(this).on('click',function(){
+ 			$('.selestplace-selected').css('display','block')
+ 								.find('h3').html($(this).text());
+ 		});
+ 		
+
+ 	});
+});
+// Mapping list of Auckland suburbs using auckland-suburbs.js data
+let suburbs = _(data.features).map(function(suburb){
+	return {
+		suburbId: suburb.properties.OBJECTID,
+		name: suburb.properties.BOARD
+	}
+});
+console.log(suburbs);
+// Creates drop-down list with Auckland suburbs
+let dropDown = new Vue({
+	el:".selectplace-wrapper",
+	data: {
+		suburbs: []
+	},
+	methods:{
+		loadSuburbs: function(){
+			this.suburbs = suburbs;
+		}
+	},
+	mounted: function(){
+		this.loadSuburbs();
+	}
 });
 
 
