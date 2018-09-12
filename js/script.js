7c125c6de1c6fb0b7668d36bcb98cb8f15a8b40e
Catriona ting yan Chan <845870007@cit3-501m-11.acg.ac.nz>
2018-09-12 15:47:45 +1200

    cat branch

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
 // Map
 
-$(()=>{
- 	
- 	var center = {lat: -45.031449, lng: 168.661904};
- 	var map = L.map('map').setView(center, 17);
+const version = '?v=20170901';
+const clientid = '&client_id=FJ1LAHN55PLM4LC1IZYPNJ3IGKZFURLHEWLE3J4JNEKF4WPN';
+const clientSecret = '&client_secret=PXXLXUDC1R2U02UDCC5M0YWVLNIPXS2N3M1OXWYUHUOQC22Q';
+const key = version + clientid + clientSecret;
 
- 	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(map);
+
+
+
+$(()=>{
+
+	var potato = {lat: -36.86667, lng: 174.76667}
+
+	var map = L.map('map').setView(potato, 13);
+	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(map);
+
+ 	var center = $('.dropdown-item').on('click', function(){
+
+		var location = [];
+ 		var lat = $(this).data('lat');
+ 		var lng = $(this).data('lng');
+
+ 		var latlng = {lat, lng};
+
+		map.setView(new L.LatLng(lat, lng), 13);
+
+
+	});
+
+
 });
 
 
