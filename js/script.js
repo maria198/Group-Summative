f70082f07419f4bd747d89d583ca8c84899daa9e
Catriona ting yan Chan <845870007@cit3-501m-11.acg.ac.nz>
2018-09-16 21:36:45 +1200

    Touched up events, make global variables, venues array, latlng global

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ const clientid = '&client_id=FJ1LAHN55PLM4LC1IZYPNJ3IGKZFURLHEWLE3J4JNEKF4WPN';
 const clientSecret = '&client_secret=PXXLXUDC1R2U02UDCC5M0YWVLNIPXS2N3M1OXWYUHUOQC22Q';
 const key = version + clientid + clientSecret;
 
+let latCurrent = [];
+let lngCurrent = [];
 
-var latCurrent = [];
-var lngCurrent = [];
-var choiceCurrent = [];
+let choiceCurrent = [];
 
-let choicesList = [];
-
+let venueCurrent = [];
 // Loading page animation
 // Effect of loading 
 var loading = anime({
@@ -17,9 +16,9 @@ var loading = anime({
 	opacity: function(el, i, l) { let d = 0.25; return 1 - d*i; },
 	// translateX: [0,0],
 	easing: [.17,.89,.8,.91],
-	loop: 3,
-	duration: 1500,
-	delay: function(el, i, l) { return i * 400; }
+	loop: 10,
+	duration: 800,
+	delay: function(el, i, l) { return i * 300; }
 });
 
 // Map & Page changing animation
@@ -30,78 +29,87 @@ $(()=>{
 	var map = L.map('map',{zoomControl:true,scrollWheelZoom:false}).setView(defaultLocation, 13);
 	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(map);
 
+	// Starts off by hiding all except the loading page
 	$('.section-0').nextAll().hide();
 
- 	var defaultToDropDown = $('.section-0').on('click',function(){
- 		$(this).hide();
- 		$(this).next().toggle();
- 	});
+	// Loads next page after 3 seconds
+	setTimeout(function(){ 
+		$('.section-0').hide();
+		$('.section-0').next().show(); 
+	}, 2000);
 
-
+	// Select location to reposition map
  	var locateMe = $('.dropdown-item').on('click', function(){
 
  		var lat = $(this).data('lat');
  		var lng = $(this).data('lng');
 
-		map.setView(new L.LatLng(lat, lng), 13);
+		latCurrent = lat;
+		lngCurrent = lng;
+
+		map.setView(new L.LatLng(latCurrent, lngCurrent), 13);
 
 	    $('.location-title h3, .dropdown-toggle').text($(this)[0].innerText);
-	    latCurrent.push(lat);
-	    lngCurrent.push(lng);
 
-	});
- 	
- 	if (('#dropdownMenu2').innerHTML !== 'Locations') {
+	    // After I pick a location, I click and it moves to the next page
  		$('.explore').on('click',function(){
 	 		
 	 		$(this).closest('section').next().slideDown();
 	 		$(this).closest('section').hide();
 		});
- 	}
 
- 	var chooseChoice = $('.choices').on('click',function(){
- 		var choice = $(this).data('choice');
-		choiceCurrent.push(choice);
+ 		// Select choice to go to after selecting map location
+		$('.choices').on('click',function(){
+	 		var choice = $(this).data('choice');
+			choiceCurrent = choice;
+	 		$('.location-choice h3').text($(this)[0].innerText);
 
- 		$('.location-choice h3').text($(this)[0].innerText);
+	 		$(this).closest('section').next().slideDown();
+	 		$(this).closest('section').hide();
 
- 		$(this).closest('section').next().slideDown();
- 		$(this).closest('section').hide();
+			let urlProjects = 'https://api.foursquare.com/v2/venues/explore'+key+'&ll='+latCurrent+','+lngCurrent+'&section='+this.choiceCurrent;
 
-						
-		let urlProjects = 'https://api.foursquare.com/v2/venues/explore'+key+'&ll='+latCurrent+','+lngCurrent+'&section='+this.choiceCurrent;
+			$.ajax({
+				url: urlProjects,
+				dataType: 'jsonp',
+				success: function(res){
 
-		$.ajax({
-			url: urlProjects,
-			dataType: 'jsonp',
-			success: function(res){
+					var data = res.response.groups[0].items;
+					// console.log(data);
+					var venues = _(data).map(function(item){
+						// console.log(item);
+						return {
+							venueid:item.venue.id,
+							name:item.venue.name,
+							address: item.venue.location.address,
+							latlng: {lat:item.venue.location.lat,lng:item.venue.location.lng},
+							genre:item.venue.categories["0"].name
+						}
+					});
+					venueCurrent = [];
+					venueCurrent.length = 0;
+					venueCurrent = venues;
+					console.log(venueCurrent);
+				},
 
-				var data = res.response.groups[0].items;
-				// console.log(data);
-				var venues = _(data).map(function(item){
-					return {
-						venueid:item.venue.id,
-						name:item.venue.name,
-						address: item.venue.location.address,
-						latlng: {lat:item.venue.location.lat,lng:item.venue.location.lng},
-						genre:item.venue.categories["0"].name
-					}
-				});
-				choiceList.clear();
-				choicesList.push(venues);
-				console.log(choicesList[0]);
-			}
-		});
- 	});
+			});
 
+	 	});
+
+	});
+ 	
+ 	// Clicking on logo returns the user to the choose location page
  	$('.logo').on('click',function(){
  		$('.section-2').slideDown();
  		$(this).closest('section').hide();
  	});
+
+ 	// Clicking on back btn returns user to previous section
  	$('.section .back-btn').on('click',function(){
 
  		$(this).closest('section').prev().slideDown();
  		$(this).closest('section').hide();
+ 		venues.clear();
 
  	});
 
