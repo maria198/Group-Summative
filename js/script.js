e01d92fca9a388573f57470afbaba05c61dc4485
maria trepakova <932400610@cit3-501m-08.acg.ac.nz>
2018-09-18 16:14:08 +1200

    comment out desktop vertion, polyline for selected suburb

--- js/script.js
+++ js/script.js
@@ -16,10 +16,14 @@ $('section#loading-page').delay(4800).fadeOut(1000);
 // Mapping list of Auckland suburbs using auckland-suburbs.js data
 let suburbs = _(data.features).map(function(suburb){
 	return {
-		suburbId: suburb.properties.OBJECTID,
-		name: suburb.properties.BOARD
+		suburbId: suburb.properties.OBJECTID-1,
+		name: suburb.properties.BOARD,
+		geometry: _(suburb.geometry.coordinates[0]).map(function(point){
+			return {lat:point[1],lng:point[0]}
+		})
 	}
 });
+console.log(data.features);
 console.log(suburbs);
 // Creates drop-down list with Auckland suburbs
 let dropDown = new Vue({
@@ -37,18 +41,38 @@ let dropDown = new Vue({
 	}
 });
 
+//FourSquare Client Id,key
+const version = '?v=20170901';
+const clientid = '&client_id=FP1I4WJSN14C1CP41SACQI3F1KSH4I0IJDO1IYKCKZBDKZIO';
+const clientSecret = '&client_secret=CJHT5MHLZSEAU2NXKOT1IVTQJAGG1TH1C11VAPK5DFIYPRVC';
+const key = version + clientid + clientSecret;
+
 // JQuery
 $(()=>{
 
 	//Drop-down list, select suburb
 	var suburbSelected = '';
+	var suburbSelectedId = 0;
+	var oSuburb;
+
 	$('.dropdown-item-mobile').each(function(){
  		$(this).on('click',function(){
  			suburbSelected = $(this).text();
  			$('#dropdown-header-mobile').html(suburbSelected);
+ 			suburbSelectedId = $(this).data('suburbid');
+
+ 			oSuburb = suburbs[suburbSelectedId];
+ 			console.log(oSuburb);
  		});
  	});
  	
+
+ 	// Map-mobile
+ 	var center = {lat: -45.031449, lng: 168.661904};
+ 	var map = L.map('map').setView(center, 17);
+ 	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(map);
+ 	var layerGroup = L.layerGroup().addTo(map);
+
  	// Navigation
  	// Explore button, from section2 to section3, change header in section3
  	$('#explore').on('click', function(){
@@ -62,6 +86,15 @@ $(()=>{
  			$('.location-title').find('h1').text(suburbSelected);
 	 		$('.current-section').removeClass('current-section');
 	 		$('.section-3').addClass('current-section');
+
+	 		layerGroup.clearLayers();
+
+	 		var oSuburbPolyline = L.polyline( oSuburb.geometry, {
+				color: '#FE5F55',
+				weight: 2
+			});
+			layerGroup.addLayer(oSuburbPolyline);
+			map.fitBounds(oSuburbPolyline.getBounds());
  		}
  	});
  	// From section3 to section4 (map)
@@ -83,18 +116,12 @@ $(()=>{
  		$('.current-section').removeClass('current-section');
  		$('.section-3').addClass('current-section');
  	});
-
- 	// Map-mobile
- 	var center = {lat: -45.031449, lng: 168.661904};
- 	var mapMobile = L.map('map').setView(center, 17);
-
- 	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(mapMobile);
-
+ 	
  	// Map-desktop
- 	var center = {lat: -45.031449, lng: 168.661904};
- 	var mapDesktop = L.map('map-desktop').setView(center, 17);
+ 	// var center = {lat: -45.031449, lng: 168.661904};
+ 	// var mapDesktop = L.map('map-desktop').setView(center, 17);
 
- 	L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(mapDesktop);
+ 	// L.tileLayer('https://api.mapbox.com/styles/v1/mary-trepakova/cjkna5n1g221w2tmt0g2tsz5o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyeS10cmVwYWtvdmEiLCJhIjoiY2pra2V6cHRzMDEzbDNqczc5NjF0aWptbiJ9.f52j7_rFo6_WhBh3aD3QKw').addTo(mapDesktop);
 
 });
 
